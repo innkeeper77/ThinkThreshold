c4ae05651e1fde735b30fce23d35d7a618e9d8a3
innkeeper77 <30507952+innkeeper77@users.noreply.github.com>
2017-11-13 23:17:45 -0700

    Update ThinkThreshold.ps1
    Changed Wow6432Node to WOW6432Node. May have changed at Lenovo, may have been a bug in my software. Verified works on X1Y2.

--- ThinkThreshold.ps1
+++ ThinkThreshold.ps1
@@ -7,30 +7,30 @@
 # This WILL NOT WORK unless you have the latest lenovo companion software installed. The registry location changed in that update.
 # Please submit a bug report on github if this script does not work, or stops working. Software updates can break it, and that is beyond my control. 
 
-$BattSerial = "L4NJ78S03E2" #YOU MUST CHANGE THIS FOR YOUR SPECIFIC LAPTOP - see readme
+$BattSerial = "L4NJ78S03E2" #YOU MUST CHANGE THIS FOR YOUR SPECIFIC LAPTOP - Battery serial number as shown in Lenovo Companion battery info. If you have two batteries, you can run two copies of the script and set them differently as desired.
 $StartP = "85"
 $StopP = "90"
 
-$val = Get-ItemProperty -Path hklm:software\Wow6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStartControl"
+$val = Get-ItemProperty -Path hklm:software\WOW6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStartControl"
 if($val.ChargeStartControl -ne 1)
 {
- set-itemproperty -Path hklm:software\Wow6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStartControl" -value 1
+ set-itemproperty -Path hklm:software\WOW6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStartControl" -value 1
 }
 
-$val = Get-ItemProperty -Path hklm:software\Wow6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStopControl"
+$val = Get-ItemProperty -Path hklm:software\WOW6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStopControl"
 if($val.ChargeStopControl -ne 1)
 {
- set-itemproperty -Path hklm:software\Wow6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStopControl" -value 1
+ set-itemproperty -Path hklm:software\WOW6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStopControl" -value 1
 }
 
-$val = Get-ItemProperty -Path hklm:software\Wow6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStartPercentage"
+$val = Get-ItemProperty -Path hklm:software\WOW6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStartPercentage"
 if($val.ChargeStartPercentage -ne $StartP)
 {
- set-itemproperty -Path hklm:software\Wow6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStartPercentage" -value $StartP
+ set-itemproperty -Path hklm:software\WOW6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStartPercentage" -value $StartP
 }
 
-$val = Get-ItemProperty -Path hklm:software\Wow6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStopPercentage"
+$val = Get-ItemProperty -Path hklm:software\WOW6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStopPercentage"
 if($val.ChargeStopPercentage -ne $StopP)
 {
- set-itemproperty -Path hklm:software\Wow6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStopPercentage" -value $StopP
+ set-itemproperty -Path hklm:software\WOW6432Node\Lenovo\PWRMGRV\ConfKeys\Data\$BattSerial -Name "ChargeStopPercentage" -value $StopP
 }
